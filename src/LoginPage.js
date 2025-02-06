d6f866d99239450f4a6d88975ae1e5414a9fc292
Alexa L <92478365+AlexaLopez-Wz@users.noreply.github.com>
2025-02-06 08:32:06 -0600

    prettier fixes
    prettier fixes

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,14 +1,14 @@
-class LoginPage{
-    constructor(page){
-        this.Username = page.getByTestId("username");
-        this.Password = page.getByTestId("password");
-        this.LoginSubmitButton = page.getByTestId("login-button");
-    }
+class LoginPage {
+  constructor(page) {
+    this.Username = page.getByTestId("username");
+    this.Password = page.getByTestId("password");
+    this.LoginSubmitButton = page.getByTestId("login-button");
+  }
 
-    async sumbitLoginForm(username, password){
-        await this.Username.fill(username);
-        await this.Password.fill(password);
-        await this.LoginSubmitButton.click();
-    }
+  async sumbitLoginForm(username, password) {
+    await this.Username.fill(username);
+    await this.Password.fill(password);
+    await this.LoginSubmitButton.click();
+  }
 }
 export default LoginPage;
